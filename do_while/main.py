ce0d4f87021085b67d493858cf480c61420af5e1
anirudha44 <anirudhakhuntia44@gmail.com>
2022-07-12 23:07:44 +0530

    first commit

--- do_while/main.py
+++ do_while/main.py
@@ -1,15 +1,11 @@
 n = 0
 while True:
     n = n + 1
-    print(f"Is {n} even or odd?")
-    if n <=10:
+    print(f"Is {n} odd or even?")
+    if n<=10:
         if n%2 == 0:
-            print(f"{n} is even")
+            print(f"{n} is even.")
         else:
-            print(f"{n} is odd")
+            print(f"{n} is odd.")
     else:
         break
-
-
-
-
